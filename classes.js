69cff2162aeb2f25bd322a60e29e19ee9126f252
Alec Lownes <alec.lownes@gmail.com>
2016-06-27 18:03:58 -0400

    Add moveTowards method to It

--- classes.js
+++ classes.js
@@ -3,6 +3,8 @@
 
 let uuid = require('node-uuid');
 
+const SPEED_PER_TICK = 84;//meters/5minutes
+
 class User {
 
 }
@@ -48,8 +50,48 @@ class It {
       return false;
     }
   }
+
+  moveTowards(user){
+    /*
+    aa = sin((1−f)⋅δ) / sin δ
+    b = sin(f⋅δ) / sin δ
+    x = aa ⋅ cos φ1 ⋅ cos λ1 + b ⋅ cos φ2 ⋅ cos λ2
+    y = aa ⋅ cos φ1 ⋅ sin λ1 + b ⋅ cos φ2 ⋅ sin λ2
+    z = aa ⋅ sin φ1 + b ⋅ sin φ2
+    φi = atan2(z, √x² + y²)
+    λi = atan2(y, x)
+    */
+    let distance = this.getDistanceToUser(user);
+    if (distance < SPEED_PER_TICK){
+      this.lat = user.lat;
+      this.lon = user.lon;
+      return;
+    }
+    let φ1 = toRadians(this.lat);
+    let φ2 = toRadians(user.lat);
+    let λ1 = toRadians(this.lon);
+    let λ2 = toRadians(user.lon);
+    let Δφ = toRadians(user.lat-this.lat);
+    let Δλ = toRadians(user.lon-this.lon);
+    let a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    let f = SPEED_PER_TICK/distance;
+    let aa = Math.sin((1-f)*c)/Math.sin(c);
+    let b = Math.sin(f*c)/Math.sin(c);
+    let x = aa * Math.cos(φ1) * Math.cos(λ1) + b * Math.cos(φ2) * Math.cos(λ2);
+    let y = aa * Math.cos(φ1) * Math.sin(λ1) + b * Math.cos(φ2) * Math.sin(λ2);
+    let z = aa * Math.sin(φ1) + b * Math.sin(φ2);
+    this.lat = toDegrees(Math.atan2(z, Math.sqrt(x*x+y*y)));
+    this.lon = toDegrees(Math.atan2(y, x));
+  }
 }
 
 function toRadians(num){
   return num/360*Math.PI*2;
+}
+
+function toDegrees(num){
+  return num/(Math.PI*2)*360;
 }
